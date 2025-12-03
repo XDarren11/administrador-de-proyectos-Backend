bbc6361d856b4856b00a3c2397314a630e00e88e
XDarren11 <jhosiescobar@gmail.com>
2025-12-02 23:08:03 -0600

    se termina de armar el crud ya con las funciones de eliminar, actualizar, buscar por un solo id

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { body } from 'express-validator'
+import { body, param } from 'express-validator'
 import { ProjectController } from "../controllers/ProjectController";
 import { handleInputError } from '../middleware/validation';
 
@@ -16,7 +16,30 @@ router.post('/',
     ProjectController.createProjects
 )
 
-
 router.get('/', ProjectController.getAllProjects)
+
+router.get('/:id', 
+    param('id').isMongoId().withMessage('ID no valido'), 
+    handleInputError,
+    ProjectController.getProjectById
+)
+
+router.put('/:id', 
+    param('id').isMongoId().withMessage('ID no valido'), 
+    body('projectName')
+        .notEmpty().withMessage('El Nombre del Proyecto es Obligatorio'),
+    body('clientName')
+        .notEmpty().withMessage('El Nombre del Cliente es Obligatorio'),
+    body('description')
+        .notEmpty().withMessage('La Descripcion del Proyecto es Obligatoria'),    
+    handleInputError,
+    ProjectController.updateProject
+)
+
+router.delete('/:id', 
+    param('id').isMongoId().withMessage('ID no valido'), 
+    handleInputError,
+    ProjectController.deleteProject
+)
 
 export default router
